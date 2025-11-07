c578437dc97a1224845a24826dca5ab221a07979
zenay910 <yanezhjs@gmail.com>
2025-11-07 06:38:58 -0700

    working on the form

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,5 +1,49 @@
-export default function createForm() {
-    return (
-        <div className="text-2xl">This is the page to create new products</div>
-    )
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormData = {
+  title: string;
+  brand: string;
+  price: Int16Array;
+  model_number: string;
+};
+
+export default function CreateProductForm() {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+
+  return (
+    <div>
+      {/*Page title */}
+      <div className="text-2xl">This is the page to create new products</div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Section 1: title, brand, price, model_number */}
+        <label>Title</label>
+        <input {...register("title")} />
+
+        <label>Brand</label>
+        <input {...register("brand")} />
+
+        <label>Price</label>
+        <input {...register("price")} />
+
+        <label>Model number</label>
+        <input {...register("model_number")} />
+
+        {/* Section 2: type, configuration, dimensions, capacity, fuel, init type */}
+
+        {/* Section 3, color, features, condition, status, description_long */}
+
+        {/* Submit button */}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
